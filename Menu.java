01b7ea6cf0b14e167e70d83a442b3dda021369ca
Bryton Tateishi <brytontateishi@gmail.com>
2026-06-28 00:06:00 -0600

    Update to group project. Removed Menu and added module to company manager

--- Menu.java
+++ Menu.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+
 public class Menu {
     public void displayMenu() {
         Scanner scnr = new Scanner(System.in);
@@ -19,8 +20,38 @@ public class Menu {
             userInput = scnr.nextInt();
 
             switch (userInput) {
-                //FIXME SET CASES FOR EACH OPTION
+                case 1:
+                    //
+                case 2:
+                    //
+                case 3:
+                    //
+                case 4:
+                    String employeeID;
+                    double newSalary;
+                    System.out.print("Enter employee ID: ");
+                    employeeID = scnr.next();
+                    System.out.print("\nSet salary for employee " + employeeID + ": ");
+                    newSalary = scnr.nextDouble();
+                    boolean found = false;
+                    for (Employee employee : empList) {
+                        if (employee.getEmployeeID() == employeeID) {
+                            employee.setSalary(newSalary);
+                            found = true;
+                            System.out.println("Salary updated for employee " + employeeID + " to: " + newSalary + ".");
+                            break;
+                        }
+                    }
+
+                    if (!found) {
+                        System.out.println("Employee not found.");
+                    }
+                case 5:
+                    //
+                case 6:
+                    //
             }
         } while (userInput != 0);
+        System.out.println("Exiting program!");
     }
 }
